e82a5f82729364d47f52e18f963ee1456b23cec5
ahmedhesein1 <ahmedhesein1790@gmail.com>
2024-11-22 14:56:11 +0200

    Adding The Routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,4 +1,8 @@
-import express from 'express';
+import express from "express";
+import * as authControl from "../controllers/authControl.js";
 const router = express.Router();
-
+router.post("/signup", authControl.signup);
+router.post("/login", authControl.login);
+router.get("/logout", authControl.protect, authControl.logout);
+router.patch('update-profile',authControl.protect,authControl.updateProfile)
 export default router;
